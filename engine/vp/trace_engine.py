4835801400308b8a313bff3141d20a4757b69c48
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2018-12-11 15:00:19 +0100

    Fixed issue, RTC was not compiled for vega

--- engine/vp/trace_engine.py
+++ engine/vp/trace_engine.py
@@ -62,8 +62,8 @@ class component(vp.component):
         config = self.get_json()
         if config is not None:
           self.impl.module.vp_trace_level(self.impl.instance, config.get_child_str('trace-level').encode('utf-8'))
-          self.reg_traces(config.get_child_str('trace'), 0)
-          self.reg_traces(config.get_child_str('event'), 1)
+          self.reg_traces(config.get('trace').get_dict(), 0)
+          self.reg_traces(config.get('event').get_dict(), 1)
 
     def pre_start(self):
         length = self.impl.module.vp_trace_exchange_max_path_len(self.impl.instance, self.max_path_len)
--- models/vendor/dolphin/rtc.py
+++ models/vendor/dolphin/rtc.py
@@ -20,4 +20,4 @@ import vp_core as vp
 
 class component(vp.component):
 
-    implementation = 'pulp.chips/gap/rtc_impl'
+    implementation = 'vendor/dolphin/rtc_impl'
