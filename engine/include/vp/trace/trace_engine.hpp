6b8e7e614e6ac28f9feb9423374f6868d1e9fc31
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2019-02-19 19:33:52 +0100

    Added possibility to dump traces to file

--- engine/include/vp/trace/trace_engine.hpp
+++ engine/include/vp/trace/trace_engine.hpp
@@ -61,7 +61,6 @@ namespace vp {
   protected:
     std::map<std::string, trace *> traces_map;
     std::vector<trace *> traces_array;
-    FILE *trace_file;
 
   private:
     void enqueue_pending(vp::trace *trace, int64_t timestamp, uint8_t *event);
