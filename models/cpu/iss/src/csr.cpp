c18a69db59e3e2c176f5c5628c27a27da7cce019
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2020-11-23 19:53:58 +0100

    [RISCV] Fixed regression, HWloops on old CSR mapping (7b0) was overlapped with debug registers

--- models/cpu/iss/src/csr.cpp
+++ models/cpu/iss/src/csr.cpp
@@ -1202,10 +1202,12 @@ bool iss_csr_read(iss_t *iss, iss_reg_t reg, iss_reg_t *value)
     case 0xC10: status = umode_read(iss, value); break;
     case 0x014: status = mhartid_read(iss, value); break;
 
+#if CSR_HWLOOP0_START != 0x7b0
     case 0x7b0: status = dcsr_read    (iss, value); break;
     case 0x7b1: status = depc_read    (iss, value); break;
     case 0x7b2: status = scratch0_read(iss, value); break;
     case 0x7b3: status = scratch1_read(iss, value); break;
+#endif
 
 #ifdef CSR_STACK_CONF
     case CSR_STACK_CONF:  status = stack_conf_read(iss, value); break;
@@ -1358,10 +1360,12 @@ bool iss_csr_write(iss_t *iss, iss_reg_t reg, iss_reg_t value)
     case 0x311: return mscounteren_write(iss, value);
     case 0x312: return mhcounteren_write(iss, value);
 
+#if CSR_HWLOOP0_START != 0x7b0
     case 0x7b0: return dcsr_write    (iss, value);
     case 0x7b1: return depc_write    (iss, value);
     case 0x7b2: return scratch0_write(iss, value);
     case 0x7b3: return scratch1_write(iss, value);
+#endif
 
 #ifdef CSR_STACK_CONF
     case CSR_STACK_CONF:  return stack_conf_write(iss, value); break;
