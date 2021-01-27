959f00505a0fdf6b85ca09f3fb1ab507bf87b0bb
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2021-01-27 18:32:56 +0100

    [I2S TESTBENCH] Fixed an issue, ext clk is now set to 0 when setup is done to have valid value

--- models/devices/testbench/i2s_verif.cpp
+++ models/devices/testbench/i2s_verif.cpp
@@ -113,10 +113,10 @@ I2s_verif::I2s_verif(Testbench *top, vp::i2s_master *itf, int itf_id, pi_testben
     {
         this->ws_value = 0;
     }
-    //if (this->config.is_ext_clk)
-    //{
-    //    this->clk = 0;
-    //}
+    if (this->config.is_ext_clk)
+    {
+        this->clk = 0;
+    }
 
     this->itf->sync(this->clk, this->ws_value, this->data);
 
